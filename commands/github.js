24d0b88af85952d627764d9173b1516dddb5a2e3
kirebainchodd <itachithegay@gmail.com>
2025-08-08 23:51:44 +0600

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/vinpink2/June-md');
+    const res = await fetch('https://api.github.com/repos/kirebainchodd/W.A.BOTLA');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
