190f2436ddc3a037ce034423e2075879aeded516
kirebainchodd <itachithegay@gmail.com>
2025-08-08 22:24:48 +0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,6 @@
 const os = require('os');
+const fs = require('fs');
+const path = require('path');
 const settings = require('../settings.js');
 
 function formatTime(seconds) {
@@ -21,21 +23,41 @@ function formatTime(seconds) {
 async function pingCommand(sock, chatId, message) {
     try {
         const start = Date.now();
-        await sock.sendMessage(chatId, { text: ' *𝙹𝚄𝙽𝙴 𝙼𝙳 𝙱𝙾𝚃* ' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: ' *𝙱𝙾𝚃𝙻𝙰 𝙱𝙾𝚃* ' }, { quoted: message });
         const end = Date.now();
         const ping = Math.round((end - start) / 2);
 
         const uptimeInSeconds = process.uptime();
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
-        const botInfo = `🔸 *June Speed:* ${ping} ms`.trim();
+        const botInfo = `ʜᴀʏʏ👋
+📡𝙱𝙾𝚃𝙻𝙰 𝚒𝚜 𝙰𝚌𝚝𝚒𝚟𝚎 𝚊𝚗𝚍 𝚊𝚕𝚒𝚟𝚎
 
-        // Reply to the original message with the bot info
-        await sock.sendMessage(chatId, { text: botInfo},{ quoted: message });
+🔸 *📼𝙱𝙾𝚃𝙻𝙰 Speed:* ${ping} ms
+🔸 *🖥Uptime:* ${uptimeFormatted}
+🔸 🧠 *CPU:* 54%
+🔸 💽 *Disk:* 198G / 387G
+🔸 💾 *RAM:* 94.21MB / 3000MB
+🔸 📀 *ROM:* 612.77MB /8.00GB
+🔸 🖥️ *Host:* katabump
+🔸 🐧 *OS:* Ubuntu/google
+🔸 🎛️ *Prefix:* . 
+   ᴍᴀᴅᴇ ᴡɪᴛʜ ᴛᴀɴᴠɪʀ 
+`.trim();
 
+        const imagePath = path.join(__dirname, '../assets/alive_image.jpg');
+        if (fs.existsSync(imagePath)) {
+            const imageBuffer = fs.readFileSync(imagePath);
+            await sock.sendMessage(chatId, {
+                image: imageBuffer,
+                caption: botInfo
+            }, { quoted: message });
+        } else {
+            await sock.sendMessage(chatId, { text: botInfo }, { quoted: message });
+        }
     } catch (error) {
         console.error('Error in ping command:', error);
-        await sock.sendMessage(chatId, { text: '❌ Failed to get bot status.' });
+        await sock.sendMessage(chatId, { text: '❌ Failed to get bot status.' }, { quoted: message });
     }
 }
 
